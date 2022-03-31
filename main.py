43dd9933530640852aa7c60a190bb7737c801995
ZerosiX <95455706+ZrosiX@users.noreply.github.com>
2022-04-01 01:58:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
 
 async def on_ready():
-  await client.change_presence(status=discord.Status.online, activity=discord.Game("TESTING"))
+  await client.change_presence(status=discord.Status.online, activity=discord.Game("ONLINE"))
 
 keep_alive.keep_alive()
-client.run(os.getenv("OTQwMDc1OTE0MjEyMDMyNTY1.YgCH6w.KVGdRfHedZj2mhQI7XCDsu6Inkc"), bot=False)
+client.run(os.getenv("OTQwMDc1OTE0MjEyMDMyNTY1.YgCH6w.Iu9Ck2--wEMtZPrNbp3ZNdcRpZM"), bot=False)
